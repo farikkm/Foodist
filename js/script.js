42060908504bb0ddffc76a814ff79cf546aed7e3
farikkm <farikk.v.2@gmail.com>
2025-05-24 11:04:55 +0500

    start of calculating form

--- js/script.js
+++ js/script.js
@@ -7,46 +7,51 @@ const buttonNext = document.querySelector(".offer__slider-next");
 let currentIndex = 0;
 
 const showSlide = (index) => {
-    slides[index].classList.remove("hide");
-    slides[index].classList.add("show");
-    slides[index].classList.add("fade");
-}
+  slides[index].classList.remove("hide");
+  slides[index].classList.add("show");
+  slides[index].classList.add("fade");
+};
 
 const hideSlide = (index) => {
-    slides[index].classList.add("hide");
-    slides[index].classList.remove("show");
+  slides[index].classList.add("hide");
+  slides[index].classList.remove("show");
+  slides[index].classList.remove("fade");
+};
+
+const setNewCurrentTab = (index) => {
+  currentTab.textContent = `0${index + 1}`;
+}
+
+const increaseSlideIndex = () => {
+  currentIndex++;
+}
+
+const decreaseSlideIndex = () => {
+  currentIndex--;
 }
 
 buttonNext.addEventListener("click", function () {
-    if (currentIndex < slides.length - 1) {
-        hideSlide(currentIndex);
-        currentIndex++;
-        currentTab.textContent = `0${getCurrentIndex() + 1}`;
-        showSlide(currentIndex);
-    }
+  if (currentIndex < slides.length - 1) {
+    hideSlide(currentIndex);
+    increaseSlideIndex();
+    setNewCurrentTab(currentIndex);
+    showSlide(currentIndex);
+  }
 });
 
 buttonPrev.addEventListener("click", function () {
-    if (currentIndex > 0) {
-        hideSlide(currentIndex);
-        currentIndex--;
-        currentTab.textContent = `0${getCurrentIndex() + 1}`;
-        showSlide(currentIndex)
-    }
-})
+  if (currentIndex > 0) {
+    hideSlide(currentIndex);
+    decreaseSlideIndex();
+    setNewCurrentTab(currentIndex);
+    showSlide(currentIndex);
+  }
+});
 
-for (let i = 0; i < slides.length; i++) {
-    const slide = slides[i];
-    hideSlide(i)
-}
-
-function getCurrentIndex() {
-    return currentIndex;
-}
+slides.forEach((_, index) => hideSlide(index));
 
 showSlide(currentIndex);
-currentTab.textContent = `0${currentIndex + 1}`;
-
+setNewCurrentTab(currentIndex);
 
 // ================== GENDERS ================== //
 
@@ -54,13 +59,56 @@ const genders = [...document.getElementById("gender").children];
 let activeGender = null;
 
 function setActiveGender(gender) {
-    if (gender.classList.contains("calculating__choose-item_active")) return;
+  if (gender.classList.contains("calculating__choose-item_active")) return;
 
-    genders.forEach(el => el.classList.remove("calculating__choose-item_active"));
-    gender.classList.add("calculating__choose-item_active");
-    activeGender = gender.dataset.gender;
+  genders.forEach((el) =>
+    el.classList.remove("calculating__choose-item_active")
+  );
+  gender.classList.add("calculating__choose-item_active");
+  activeGender = gender.dataset.gender;
 }
 
-genders.forEach(gender => {
-    gender.addEventListener("click", () => setActiveGender(gender));
-})
+genders.forEach((gender) => {
+  gender.addEventListener("click", () => setActiveGender(gender));
+});
+
+// ================== CALCULATING FORM ================== //
+
+const calculatingInputs = document.getElementById("calculating-form").children;
+
+let measurements = { height: 0, weight: 0, age: 0 };
+
+for (const input of calculatingInputs) {
+  input.addEventListener("input", (e) => {
+    const { id, value } = e.target; // { target: { value: "asdf" } }
+
+    if (id in measurements) {
+      console.log(id);
+
+      measurements[id] = parseInt(value);
+    }
+
+    console.log(measurements);
+    
+  });
+}
+
+// for (let index = 0; index < calculatingInputs.length; index++) {
+//   const input = calculatingInputs[index];
+
+//   input.addEventListener("input", (e) => {
+//     if (input.matches("#height")) {
+//       height = Number(e.target.value);
+//     }
+//     if (input.matches("#weight")) {
+//       weight = Number(e.target.value);
+//     }
+//     if (input.matches("#age")) {
+//       age = Number(e.target.value);
+//     }
+
+//     console.log("Height: ", height);
+//     console.log("Weight: ", weight);
+//     console.log("Age: ", age);
+//   });
+// }
